9d361552b58266f29bcee0feb271e4752ed58226
gary-stroup-developer <stroup.uxuidesign@gmail.com>
2024-03-17 22:30:34 -0700

    add CreateForm due to issues with displaying data. Add form validation with react hook form

--- src/features/events/form/CreateForm.tsx
+++ src/features/events/form/CreateForm.tsx
@@ -1,15 +1,12 @@
-import { useNavigate, useParams } from "react-router-dom"
+import { useNavigate} from "react-router-dom"
 import { Button, Form, Header, Segment } from "semantic-ui-react"
-import { useAppDispatch, useAppSelector } from "../../../app/store/hooks";
-import { updateEvent } from "../eventSlice";
+import { useAppDispatch } from "../../../app/store/hooks";
+import { createEvent} from "../eventSlice";
+import { createId } from "@paralleldrive/cuid2";
 import { FieldValues, useForm } from "react-hook-form";
 
+const CreateForm = () => {
 
-
-const EventForm = () => {
-
-    let { id } = useParams();
-    const event = useAppSelector(state => state.events.events.find(evt => evt.id === id));
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
     const { register, handleSubmit, formState: { errors, isValid, isSubmitting } } = useForm({
@@ -18,46 +15,45 @@ const EventForm = () => {
 
 
     function onSubmit(data: FieldValues) {
-        dispatch(updateEvent({...event, ...data})) 
+        const id = createId();
+        dispatch(createEvent({...data, id, hostedBy:'Gary', attendees:[], hostPhotoURL:'https://randomuser.me/api/portraits/men/20.jpg'}));
         navigate(`/events/${id}`);
     }
-
-    return (
-        <Segment clearing>
-            <h3>{event ? '' : 'Data not properly loaded'}</h3>
-            <Header content={'Event Details'} sub color="teal" />
+  return (
+    <Segment clearing>
+            <Header content={'Create Event'} />
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <Form.Input 
                     placeholder="Event title"
-                    defaultValue={event?.title}
+                    defaultValue={''}
                     {...register('title',{required: true})}
                     error={errors.title && 'Title is required'}
                 />
 
                 <Form.Input 
                     placeholder="Category"
-                    defaultValue={event?.category}
+                    defaultValue={''}
                     {...register('category',{required: true})}
                     error={errors.category && 'Field is required'}
                 />
                     
-                <Form.TextArea 
+                <Form.Input 
                     placeholder="Description"
-                    defaultValue={event?.description}
+                    defaultValue={''}
                     {...register('description',{required: true})}
                     error={errors.description && 'Field is required'}
                 />
-                <Header sub content='Location details' color="teal" />
+        
                 <Form.Input 
                     placeholder="City"
-                    defaultValue={event?.city}
+                    defaultValue={''}
                     {...register('city',{required: true})}
                     error={errors.city && 'Field is required'}
                 />
               
                 <Form.Input 
                     placeholder="Venue"
-                    defaultValue={event?.venue}
+                    defaultValue={''}
                     {...register('venue',{required: true})}
                     error={errors.venue && 'Field is required'}
                 />
@@ -65,7 +61,7 @@ const EventForm = () => {
                 <Form.Input 
                     type='date'
                     placeholder="Date" 
-                    defaultValue={event?.date}
+                    defaultValue={''}
                     {...register('date',{required: true})}
                     error={errors.date && 'Field is required'}
                 />
@@ -73,8 +69,7 @@ const EventForm = () => {
                 <Button loading={isSubmitting} disabled={!isValid} type="submit" floated="right" positive content="Submit" />
                 <Button disabled={isSubmitting} type="button" floated="right" content="Cancel" />
             </Form>
-          
         </Segment>
-    )
+  )
 }
-export default EventForm
+export default CreateForm
